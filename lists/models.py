50a3f394b4b31672d6101437a8b6febfd5151bc6
Sergio Polanco <polancosergio8@gmail.com>
2023-09-23 14:32:59 -0300

    Include name field to list model, include updated at field to product, brand and unit models

--- lists/models.py
+++ lists/models.py
@@ -5,6 +5,7 @@ from products.models import Product
 class List(models.Model):
     products = models.ManyToManyField(Product)
     created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
 
     class Meta:
         db_table: str = "shopping_list"
